4ec146a74bbc09a2cf7142239deaec5b3199a3c9
Matthew Nguyen <76631936+mattwhen@users.noreply.github.com>
2024-02-03 23:13:13 -0600

    Adjusted font size for small, med, and large devices in footer

--- app/components/Footer/Footer.jsx
+++ app/components/Footer/Footer.jsx
@@ -36,29 +36,29 @@ const Footer = () => {
 	];
 
 	return (
-		<footer className='bg-gray bg-stone-300 bottom-0 mt-28 h-[248px]'>
+		<footer className='bg-gray bg-stone-300 bottom-0 mt-28 h-[300px]'>
 			<div className='footerContainer flex items-start justify-evenly py-4'>
 				{footerLinks.map(({ id, title, links }) => {
 					return (
 						<div>
-							<h2 className='text-2xl'>{title}</h2>
+							<h2 className='text-sm mb-3 md:text-xl lg:text-2xl'>{title}</h2>
 							<ul className='flex flex-col'>
-								<li className='cursor-pointer hvr-fade hover:text-yellow'>{links[0]}</li>
-								<li className='cursor-pointer hvr-fade hover:text-yellow'>{links[1]}</li>
-								<li className='cursor-pointer hvr-fade hover:text-yellow'>{links[2]}</li>
-								<li className='cursor-pointer hvr-fade hover:text-yellow'>{links[3]}</li>
+								<li className='text-xs cursor-pointer hvr-fade hover:text-yellow md:text-md lg:text-lg'>{links[0]}</li>
+								<li className='text-xs cursor-pointer hvr-fade hover:text-yellow md:text-md lg:text-lg'>{links[1]}</li>
+								<li className='text-xs cursor-pointer hvr-fade hover:text-yellow md:text-md lg:text-lg'>{links[2]}</li>
+								<li className='text-xs cursor-pointer hvr-fade hover:text-yellow md:text-md lg:text-lg'>{links[3]}</li>
 							</ul>
 						</div>
 					);
 				})}
 			</div>
-			<ul className='flex justify-center items-center space-x-5 mb-4'>
+			<ul className='flex justify-center items-center space-x-5 my-4'>
 				<li className='text-center rounded-full'>
-					<FaGithub className='text-3xl cursor-pointer m-auto hover:text-black' />
+					<FaGithub className='text-2xl lg:text-3xl cursor-pointer m-auto hover:text-black' />
 				</li>
-				<FaLinkedin className='text-3xl cursor-pointer m-auto hover:text-black' />
-				<FaTiktok className='text-3xl cursor-pointer m-auto hover:text-black' />
-				<FaXTwitter className='text-3xl cursor-pointer m-auto hover:text-black' />
+				<FaLinkedin className='text-2xl lg:text-3xl cursor-pointer m-auto hover:text-black' />
+				<FaTiktok className='text-2xl lg:text-3xl cursor-pointer m-auto hover:text-black' />
+				<FaXTwitter className='text-2xl lg:text-3xl cursor-pointer m-auto hover:text-black' />
 			</ul>
 			<div className='flex justify-center'>
 				<p>© 2023 Matthew A. Nguyen. All Rights Reserved.</p>
