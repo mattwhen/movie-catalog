0274190fe17949ea2e4b958c4d37a2f257828bbc
Matthew Nguyen <76631936+mattwhen@users.noreply.github.com>
2024-08-20 23:37:38 -0500

    Disabled Image optimization from Vercel due to 402 errors in production

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 module.exports = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: '**.themoviedb.org', // To protect your application from malicious users, configuration is required in order to use external images. 
-        },
-      ],
-    },
+  images: {
+    unoptimized: true,
+  },
+    // images: {
+    //   remotePatterns: [
+    //     {
+    //       protocol: 'https',
+    //       hostname: '**.themoviedb.org', // To protect your application from malicious users, configuration is required in order to use external images. 
+    //     },
+    //   ],
+    // },
   }
